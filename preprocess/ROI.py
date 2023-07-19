a528f107f9a5a2cbb81db358da69265a35b8e1eb
RodrigofrBastos <rodrigo.bastos@aln.senaicimatec.edu.br>
2023-07-19 17:54:11 -0300

    added updates to ROI.py

--- preprocess/ROI.py
+++ preprocess/ROI.py
@@ -3,11 +3,8 @@ import numpy as np
 
 
 # Carrega a imagem
-<<<<<<< HEAD
-img = cv2.imread('dataset/worked/Placa-Preta-2.webp')
-=======
-img = cv2.imread('/home/rod/mapvision-ras/images/placa1.jpeg')
->>>>>>> parent of 708b718 (new set of images)
+img = cv2.imread('dataset/PlacaMercosulHeader.webp')
+# img = cv2.imread('/home/rod/mapvision-ras/images/placa1.jpeg')
 # img = cv2.imread('dataset/fiesta-noite(2).jpeg')
 # Converte a imagem para tons de cinza
 gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
@@ -26,15 +23,29 @@ for contour in contours:
     perimeter = cv2.arcLength(contour, True)
     approx = cv2.approxPolyDP(contour, 0.02 * perimeter, True)
     
-    # A placa do carro tem geralmente quatro lados
-    if len(approx) == 4:
-        x, y, w, h = cv2.boundingRect(contour)
-        roi = img[y:y+h, x:x+w]  # Extrai a ROI
+    # A placa do carro tem quatro lados
+    if len(approx) == 4: 
+        original_plate_size = 0.325
         
-        # Desenha o retângulo ao redor da placa
+        x, y, w, h = cv2.boundingRect(contour)
+        print("{}|{}|{}|{}".format(x, y, w, h))
         cv2.rectangle(img, (x,y), (x+w,y+h), (0,255,0), 2)
-        break  # Para a iteração quando a placa for encontrada
-
+        plate_size_height = h-y
+        plate_size_width = w-x
+        size = plate_size_height/plate_size_width
+        print("Real size = 0.325 || Plate size = {}".format(size)) 
+        roi = img[y:y+h, x:x+w]
+        
+        if np.isclose(size,original_plate_size,atol=0.09):
+            # Extrai a ROI
+            roi = img[y:y+h, x:x+w]   
+            cv2.rectangle(img, (x,y), (x+w,y+h), (0,255,0), 2)
+            # print("Real size = 0.325 || Plate size = {}".format(size))  
+            print("Recognized ROI")               
+            break
+        else:
+            print("no plate was recognized")
+        
 # Exibe a imagem com a ROI identificada
 cv2.imshow("ROI", roi)
 cv2.waitKey(0)
